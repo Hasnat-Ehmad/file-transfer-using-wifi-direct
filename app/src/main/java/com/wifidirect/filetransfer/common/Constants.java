e7e1a91383a6066d83f361975e8060dbbfd9576f
hasnat_Ehmad <hasnat.ahmad@hotmail.com>
2022-03-10 14:47:19 +0500

    transfer all file and save in downloads

---
+++ app/src/main/java/com/wifidirect/filetransfer/common/Constants.java
@@ -0,0 +1,13 @@
+package com.wifidirect.filetransfer.common;
+
+/**
+ * @Author: leavesC
+ * @Date: 2019/11/23 17:54
+ * @Desc:
+ * @Github：https://github.com/leavesC
+ */
+public class Constants {
+
+    public static final int PORT = 1995;
+
+}
